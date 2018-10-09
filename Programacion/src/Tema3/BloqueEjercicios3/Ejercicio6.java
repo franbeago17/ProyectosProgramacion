fa247011cdc0d39d3930f7726ffecdb95fa70148
diurno <diurno@pc-aula>
2018-10-09 08:47:43 +0200

    ert

--- Programacion/src/Tema3/BloqueEjercicios3/Ejercicio6.java
+++ Programacion/src/Tema3/BloqueEjercicios3/Ejercicio6.java
@@ -5,15 +5,22 @@ import javax.swing.JOptionPane;
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
+		int numero=1;
 		String mensaje = "Introduzca numero (0 -> Terminar)";
-		int numero= Integer.parseInt(JOptionPane.showInputDialog(mensaje));
 		for (int i = 0;numero !=0;i++) {
+		 numero= Integer.parseInt(JOptionPane.showInputDialog(mensaje));
 			if (i==0){
 				System.out.println(i + " x " + numero + " = " + (i * numero));
+				
 				for (i=1;i<11;i++) {
 					System.out.println(i + " x " + numero + " = " + (i * numero));
 				}
-			}	
+			}
+			else {
+				for (i=1;i<11;i++) {
+					System.out.println(i + " x " + numero + " = " + (i * numero));
+				}
+			}
 		}
 	}
 }
