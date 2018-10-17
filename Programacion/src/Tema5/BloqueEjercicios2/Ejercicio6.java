fa272e481bf6cb652d85343309a15f60c4705b79
Ana <Ana@AnaGomez>
2018-10-17 20:15:57 +0200

    e5ytt

--- Programacion/src/Tema5/BloqueEjercicios2/Ejercicio6.java
+++ Programacion/src/Tema5/BloqueEjercicios2/Ejercicio6.java
@@ -9,26 +9,36 @@ public class Ejercicio6 {
 		System.out.println("Array Primero: ");
 		for(i=0; i<array.length;i++) {
 			array[i]=(int)Math.round(Math.random()*100);
-			System.out.println("numero["+i+"]:"+array[i]);
+			System.out.print(array[i]+"  ");
 		}
 		int n=Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de repeticiones:"));
 		int direccion=Integer.parseInt(JOptionPane.showInputDialog("Introduce 1 para ir a la derecha y 0 para ir a la izquierda:"));
 		if (direccion==1) {
 			for(j=0;j<n;j++) {
-				System.out.println("Desplazamiento "+(j+1));
+				System.out.println("\nDesplazamiento hacia la derecha "+(j+1));
 				int aux=array[array.length-1];
 				for(i=(array.length-1);i>0;i--) {
 					array[i]=array[i-1];
 				}
 				array[0]=aux;
 				for (i=0; i<array.length;i++) {
-					System.out.println("numero["+i+"]:"+array[i]);
+					System.out.print(array[i]+"  ");
 				}	
 			}
 		}
 		else {
 			if (direccion==0) {
-				
+				for(j=0;j<n;j++) {
+					System.out.println("\nDesplazamiento hacia la izquierda "+(j+1));
+					int aux2=array[0];										
+					for(i=0;i<(array.length-1);i++) {
+						array[i]=array[i+1];
+					}
+					array[array.length-1]=aux2;
+					for(i=0;i<array.length;i++) {
+						System.out.print(array[i]+"  ");
+					}
+				}		
 			}
 		}
 	}
---
+++ Programacion/src/Tema5/BloqueEjercicios3/Ejercicio1.java
@@ -0,0 +1,26 @@
+package Tema5.BloqueEjercicios3;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		int array[]= new int[150], i=0, j=0, aux=0;
+		System.out.println("Array sin oredenar: ");
+		for(i=0; i<array.length;i++) {
+			array[i]=(int)Math.round(Math.random()*1000);
+			System.out.print(array[i]+"  ");
+		}
+		for (i=2;i<array.length;i++) {
+			for (j=0;j<(array.length-1);j++) {
+				if (array[j]>array[j+1]) {
+					aux=array[j];
+					array[j]=array[j+1];
+					array[j+1]=aux;
+					}
+				}
+			}
+		System.out.println("\n\nArray ordenado: ");
+		for(i=0; i<array.length;i++) {
+			System.out.print(array[i]+"  ");
+		}
+	}
+}
