2b16ad7bd94a1e9ad8f24f82cde4083f4e6579bb
diurno <diurno@pc-aula>
2018-10-19 14:55:07 +0200

    hk

--- Programacion/src/Tema5/BloqueEjercicios3/Ejercicio1.java
+++ Programacion/src/Tema5/BloqueEjercicios3/Ejercicio1.java
@@ -3,15 +3,16 @@ package Tema5.BloqueEjercicios3;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		int array[]= new int[10], i=0, j=0, aux=0;
+		int array[]= new int[150], i=0, j=0, aux=0;
 		System.out.println("Array sin oredenar: ");
 		for(i=0; i<array.length;i++) {
 			array[i]=(int)Math.round(Math.random()*1000);
 			System.out.print(array[i]+"  ");
-		}
-		for (i=1;i<array.length;i++) {
-			for (j=0;j<(array.length-i);j++) {
+		}//Ordenacion por metodo de burbuja.
+		for (i=0;i<array.length;i++) {
+			for (j=0;j<(array.length-1);j++) {
 				if (array[j]>array[j+1]) {
+					//Intercambio de numeros.
 					aux=array[j];
 					array[j]=array[j+1];
 					array[j+1]=aux;
